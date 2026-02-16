c99fd981104f5757017da38d6362ffda55e7b3ab
“yeripallivijay” <yeripallivijay2003@gmail.com>
2026-02-16 20:24:51 +0530

    first commit

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def predict_datapoint():
     if request.method=='GET':
                    return render_template('home.html')
     else:
-                data = CustomData(
+        data = CustomData(
                     gender=request.form.get('gender'),
                     race_ethnicity=request.form.get('ethnicity'),
                     parental_level_of_education=request.form.get('parental_level_of_education'),
@@ -27,15 +27,15 @@ def predict_datapoint():
                     test_preparation_course=request.form.get('test_preparation_course'),
                     reading_score=float(request.form.get('writing_score')),
                     writing_score=float(request.form.get('reading_score'))   
-                )
-                pred_df = data.get_data_as_data_frame()
-                print(pred_df)
-                print("Before Prediction")
-                predict_pipeline = PredictPipeline()
-                print("Mid Prediction")
-                results=predict_pipeline.predict(pred_df)
-                print("after Prediction")
-                return render_template('home.html',results=results[0])
+        )
+        pred_df = data.get_data_as_data_frame()
+        print(pred_df)
+        print("Before Prediction")
+        predict_pipeline = PredictPipeline()
+        print("Mid Prediction")
+        results=predict_pipeline.predict(pred_df)
+        print("after Prediction")
+        return render_template('home.html',results=results[0])
            
 if __name__ == "__main__":
        app.run(host="0.0.0.0", port=8080)
